1bed722993066383eb3c8d6a70d52dcb0bc78531
Crisfon6 <disruptivecrisfon6@gmail.com>
2025-01-29 19:45:23 -0500

    feat: create orders

--- coffe_shop/settings.py
+++ coffe_shop/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'crispy_tailwind',
     
     'products',
-    'users'
+    'users',
+    'orders'
     
 ]
 
--- coffe_shop/urls.py
+++ coffe_shop/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/',include('products.urls')),
-    path('users/', include('users.urls'))  
+    path('users/', include('users.urls')),
+    path('orders/',include('orders.urls'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
