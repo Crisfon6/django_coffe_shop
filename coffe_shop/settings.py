54046f6f2faa5eb3c86cfe7c33694b8b695585a5
Crisfon6 <disruptivecrisfon6@gmail.com>
2025-02-07 21:39:52 -0500

    feat: change the dep the mysqlclient by pymysql

--- coffe_shop/settings.py
+++ coffe_shop/settings.py
@@ -16,6 +16,9 @@ import configparser
 import environ
 import sys
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
