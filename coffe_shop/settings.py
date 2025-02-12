b98f26d0081af8f607229054a8ac83a4bfe52cfe
Crisfon6 <disruptivecrisfon6@gmail.com>
2025-02-12 16:24:46 -0500

    feat: load static_root and media_root from .env

--- coffe_shop/settings.py
+++ coffe_shop/settings.py
@@ -189,7 +189,7 @@ STATIC_URL = "static/"
 #STATICFILES_DIRS = [
 #    os.path.join(BASE_DIR, 'static'),
 #]
-STATIC_ROOT = 'static'
+STATIC_ROOT = env("STATIC_ROOT", default=os.path.join(BASE_DIR, "static"))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -197,7 +197,7 @@ STATIC_ROOT = 'static'
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = env("MEDIA_ROOT", default=os.path.join(BASE_DIR, "media"))
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
 
